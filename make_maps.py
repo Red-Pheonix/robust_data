6b1cc4576d1d8b94081694ff7e7b1b8ccc3b54ae
Red-Pheonix <rafirafi155@gmail.com>
2024-06-24 21:45:49 +0600

    added queue maps

--- make_maps.py
+++ make_maps.py
@@ -31,18 +31,83 @@ def make_map(network_file, edgedata_file, output_file, title='Test Title', max_c
     subprocess.run(command, check=True)
 
 
-def make_maps(network_file, case_name, title='Test Title', max_color_value=50):
+def make_maps(network_file, case_name, base_title='Test Title', max_color_value=50):
     matching_files = glob.glob(f'grid4x4/**/{case_name}_edgedata.xml')
     for edgedata_file in matching_files:
         print(f"Processing {edgedata_file}...")
         output_dir = os.path.join(os.path.dirname(edgedata_file), case_name)
         os.makedirs(output_dir, exist_ok=True)
         output_file = os.path.join(output_dir, f"{case_name}.png")
+        model_name = os.path.dirname(edgedata_file).split("/")[-1]
+        title = f"{base_title} - {model_name}"
         make_map(network_file, edgedata_file, output_file, title=title, max_color_value=max_color_value)
 
 make_maps(
     network_file='grid4x4.net.xml',
     case_name='morning_1',
-    title='Case 1 Queue Length Map',
+    base_title='Case 1 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='morning_2',
+    base_title='Case 2 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='morning_3',
+    base_title='Case 3 Queue Length Map',
+    max_color_value=30
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='morning_4',
+    base_title='Case 4 Queue Length Map',
+    max_color_value=50
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='morning_5',
+    base_title='Case 5 Queue Length Map',
+    max_color_value=100
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='evening_1',
+    base_title='Case 6 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='pse_1',
+    base_title='Case 7 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='morning_1_sensor',
+    base_title='Case 8 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='combined',
+    base_title='Case 9 Queue Length Map',
+    max_color_value=40
+)
+
+make_maps(
+    network_file='grid4x4.net.xml',
+    case_name='combined_sensor',
+    base_title='Case 10 Queue Length Map',
     max_color_value=40
 )
