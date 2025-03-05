999a449a582b9aaf6c8b61e78fd7b99ac36e97f0
Red-Pheonix <rafirafi155@gmail.com>
2025-03-05 17:42:41 +0600

    added pivot table

---
+++ grid_3.py
@@ -0,0 +1,43 @@
+import os
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+
+DIRECTORY = "summary\\grid4x4\\"
+INPUT_FILE = os.path.join(DIRECTORY, "all.csv")
+INITIAL_DIRECTORY = os.path.join(DIRECTORY, "initial.csv")
+OPTIMUM_DIRECTORY = os.path.join(DIRECTORY, "optimum.csv")
+RECOVERY_DIRECTORY = os.path.join(DIRECTORY, "recovery.csv")
+DROP_DIRECTORY = os.path.join(DIRECTORY, "drop.csv")
+
+case_mapping = {
+    "robust_morning_1": 1,
+    "robust_morning_2": 2,
+    "robust_morning_3": 3,
+    "robust_morning_4": 4,
+    "robust_morning_5": 5,
+    "robust_evening_1": 6,
+    "robust_pse_1": 7,
+    "robust_sensor": 8,
+}
+
+# open the file and load into a pandas dataframe
+df = pd.read_csv(INPUT_FILE, index_col=False)
+
+df["case"] = df["case"].map(case_mapping)
+
+
+pivot_df = df.pivot(index="case", columns="model_name")
+
+
+initial_travel_time = pivot_df['initial_travel_time'].reindex(columns=["dqn", "colight", "mplight"])
+min_travel_time = pivot_df['optimum_travel_time'].reindex(columns=["dqn", "colight", "mplight"])
+recovery_time = pivot_df['recovery_time'].reindex(columns=["dqn", "colight", "mplight"])
+initial_drop = pivot_df['initial_drop'].reindex(columns=["dqn", "colight", "mplight"])
+
+initial_travel_time.to_csv(INITIAL_DIRECTORY)
+min_travel_time.to_csv(OPTIMUM_DIRECTORY)
+recovery_time.to_csv(RECOVERY_DIRECTORY)
+initial_drop.to_csv(DROP_DIRECTORY)
+
+print("done")
---
+++ ingo_3.py
@@ -0,0 +1,41 @@
+import os
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+
+DIRECTORY = "summary\\ingo\\"
+INPUT_FILE = os.path.join(DIRECTORY, "all.csv")
+INITIAL_DIRECTORY = os.path.join(DIRECTORY, "initial.csv")
+OPTIMUM_DIRECTORY = os.path.join(DIRECTORY, "optimum.csv")
+RECOVERY_DIRECTORY = os.path.join(DIRECTORY, "recovery.csv")
+DROP_DIRECTORY = os.path.join(DIRECTORY, "drop.csv")
+
+non_rl_model_names = ["maxpressure", "fixedtime"]
+
+case_mapping = {
+    "ingo_morning": 1,
+    "ingo_noon": 2,
+    "ingo_evening": 3,
+    "ingo_sensor": 4,
+}
+
+# open the file and load into a pandas dataframe
+df = pd.read_csv(INPUT_FILE, index_col=False)
+
+df["case"] = df["case"].map(case_mapping)
+
+
+pivot_df = df.pivot(index="case", columns="model_name")
+
+
+initial_travel_time = pivot_df['initial_travel_time'].reindex(columns=["dqn", "colight", "mplight"])
+min_travel_time = pivot_df['optimum_travel_time'].reindex(columns=["dqn", "colight", "mplight"])
+recovery_time = pivot_df['recovery_time'].reindex(columns=["dqn", "colight", "mplight"])
+initial_drop = pivot_df['initial_drop'].reindex(columns=["dqn", "colight", "mplight"])
+
+initial_travel_time.to_csv(INITIAL_DIRECTORY)
+min_travel_time.to_csv(OPTIMUM_DIRECTORY)
+recovery_time.to_csv(RECOVERY_DIRECTORY)
+initial_drop.to_csv(DROP_DIRECTORY)
+
+print("done")
